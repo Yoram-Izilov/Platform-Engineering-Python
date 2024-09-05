5e30adeac9e9f3b6e196b22b1e960a19e7cb2c81
NerdFromHell <izilovy@gmail.com>
2024-09-05 11:40:52 +0300

    updated help for ec2

--- ec2.py
+++ ec2.py
@@ -1,1 +1,8 @@
 import boto3
+
+def create_ec2():
+    pass
+def update_ec2():
+    pass 
+def list_ec2():
+    pass
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import argparse
+from ec2 import create_ec2, update_ec2, list_ec2
 
 parser = argparse.ArgumentParser(description='Creates AWS resources.')
 parser.add_argument('--resource',
                      type=str,
                      required=True,
                      help='Specify the AWS resource (ec2)')
-parser.add_argument('--resource',
+parser.add_argument('--linux',
                     type=str,
+                    default='ubuntu',
                     help='Specify the AWS AMI (ubuntu, amazon)')
 
 # Parse the arguments
@@ -15,6 +17,7 @@ args = parser.parse_args()
 # Access and use the --resource argument
 if args.resource == "ec2":
     print("You have selected EC2 as the resource.")
+    
 else:
     print(f"Resource {args.resource} is not recognized.")
 
